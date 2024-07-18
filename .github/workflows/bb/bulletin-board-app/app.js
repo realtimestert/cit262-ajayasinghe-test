d6a646ea306337401052d9c52e4e5857e3c0fed1
Andrew Jayasinghe <55458355+andrewjayasinghe@users.noreply.github.com>
2024-07-18 10:50:33 -0700

    Pushing to GKE

--- .github/workflows/bb/bulletin-board-app/app.js
+++ .github/workflows/bb/bulletin-board-app/app.js
@@ -2,66 +2,65 @@ new Vue({
   el: '#events',
 
   data: {
-    event: { id:'', title: '', detail: '', date: '' },
+    event: { id: '', title: '', detail: '', date: '' },
     events: []
   },
 
-  ready: function () {
+  created: function () {
     this.fetchEvents();
   },
 
   methods: {
-
     fetchEvents: function () {
-      var events = [];
       this.$http.get('/api/events')
-        .success(function (events) {
-          this.$set('events', events);
-          console.log(events);
+        .then(response => {
+          this.events = response.data;
+          console.log(this.events);
         })
-        .error(function (err) {
-          console.log(err);
+        .catch(error => {
+          console.log(error);
         });
     },
 
     addEvent: function () {
-      if (this.event.title.trim()) {
-        // Generate a unique ID for the new event
-
+      if (this.event.title.trim() && this.event.detail.trim() && this.event.date.trim()) {
+        // Generate a unique numeric ID for the new event
         this.event.id = this.generateUniqueId();
 
         this.$http.post('/api/events', this.event)
-          .success(function (res) {
+          .then(response => {
             this.events.push(this.event);
             console.log('Event added!');
             // Reset the event form
-
             this.event = { id: '', title: '', detail: '', date: '' };
           })
-          .error(function (err) {
-            console.log(err);
+          .catch(error => {
+            console.log(error);
           });
+      } else {
+        alert("All fields are required!");
       }
     },
 
     deleteEvent: function (id) {
-      if (confirm('Are you sure you want to delete this event?')) {        
-        this.$http.delete('api/events/' + id)
-          .success(function (res) {
-            console.log(res);
-            var index = this.events.find(x => x.id === id)
-            this.events.splice(index, 1);
+      if (confirm('Are you sure you want to delete this event?')) {
+        this.$http.delete('/api/events/' + id)
+          .then(response => {
+            console.log(response);
+            const index = this.events.findIndex(event => event.id === id);
+            if (index !== -1) {
+              this.events.splice(index, 1);
+            }
           })
-          .error(function (err) {
-            console.log(err);
+          .catch(error => {
+            console.log(error);
           });
       }
     },
 
-    generateUniqueId: function(){
-      return 'id-' + Date.now() + '-' + Math.random().toString(36).substr(2,9);
+    generateUniqueId: function () {
+      return Math.floor(Math.random() * 1000000); // Generates a random integer between 0 and 999999
     }
-
-
+    
   }
 });
